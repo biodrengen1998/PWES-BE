c7361f68ac8768300904bb1df2ccfd3e46f97457
Mads Harder Møller <gbp326@alumni.ku.dk>
2024-10-08 14:58:55 +0200

    added support for not having pymol installed

--- pwes/mapping/map_to_pymol.py
+++ pwes/mapping/map_to_pymol.py
@@ -1,31 +1,39 @@
-import pymol2
 import os
+try:
+    import pymol2
+except:
+    pymol = None
 
 
-def map_to_pymol(pdb_location, dict_of_clusters, n_clusters, output_dir, protein_name):
+if pymol:
     
-    with pymol2.PyMOL() as pymol:
-        pymol.cmd.load(pdb_location)
+    def map_to_pymol(pdb_location, dict_of_clusters, n_clusters, output_dir, protein_name):
         
-        for cluster in dict_of_clusters:
+        with pymol2.PyMOL() as pymol:
+            pymol.cmd.load(pdb_location)
             
-                # Select residues from the specified chain
-                chains = list(dict_of_clusters[cluster].keys())
+            for cluster in dict_of_clusters:
                 
-                n_clusters = len(dict_of_clusters)
+                    # Select residues from the specified chain
+                    chains = list(dict_of_clusters[cluster].keys())
+                    
+                    n_clusters = len(dict_of_clusters)
+                    
+                    for chain in chains:
+                        if len(dict_of_clusters[cluster][chain]) == 0:
+                            continue
+                        else:
+                            pymol.cmd.select(f"cluster_{cluster}_{chain}", f"resi {'+'.join(dict_of_clusters[cluster][chain].split('+'))} and chain {chain}")
+                            pymol.cmd.color("red", f"cluster_{cluster}_{chain}")
+                    
+                    
                 
-                for chain in chains:
-                    if len(dict_of_clusters[cluster][chain]) == 0:
-                        continue
-                    else:
-                        pymol.cmd.select(f"cluster_{cluster}_{chain}", f"resi {'+'.join(dict_of_clusters[cluster][chain].split('+'))} and chain {chain}")
-                        pymol.cmd.color("red", f"cluster_{cluster}_{chain}")
-                
-                
-            
 
+            
+            # Save the PyMOL session
+            pymol.cmd.save(os.path.join(output_dir,f"{n_clusters}", f"{protein_name}_clusters_{n_clusters}.pse"))
+else:
+    def map_to_pymol(pdb_location, dict_of_clusters, n_clusters, output_dir, protein_name):
+        print("Install pymol open-source to generate PyMOL sessions")
+        return None
         
-        # Save the PyMOL session
-        pymol.cmd.save(os.path.join(output_dir,f"{n_clusters}", f"{protein_name}_clusters_{n_clusters}.pse"))
-    
-    
